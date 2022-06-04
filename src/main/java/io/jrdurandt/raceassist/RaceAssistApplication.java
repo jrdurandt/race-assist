64916e83db567404c82c4b7af492a798b994fa68
Johannes du Randt <jrdurandt@protonmail.com>
2022-06-04 11:16:23 +0200

    Spring-board

--- src/main/java/io/jrdurandt/raceassist/RaceAssistApplication.java
+++ src/main/java/io/jrdurandt/raceassist/RaceAssistApplication.java
@@ -1,4 +1,4 @@
-package com.example.raceassist;
+package io.jrdurandt.raceassist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/io/jrdurandt/raceassist/RaceAssistApplicationTests.java
+++ src/test/java/io/jrdurandt/raceassist/RaceAssistApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.raceassist;
+package io.jrdurandt.raceassist;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
