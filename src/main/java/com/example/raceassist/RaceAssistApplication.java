65d7dd355d8dd5b0c4099f7ee4b25f99839c7c8e
Johannes du Randt <jrdurandt@protonmail.com>
2022-06-04 11:09:21 +0200

    Initial commit

---
+++ src/main/java/com/example/raceassist/RaceAssistApplication.java
@@ -0,0 +1,13 @@
+package com.example.raceassist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaceAssistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaceAssistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/raceassist/RaceAssistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.raceassist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaceAssistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
